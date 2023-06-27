7b9ffe818ad79faea36acce3c3c181585761ab2f
Crowdbotics <team@crowdbotics.com>
2023-06-27 13:43:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-dew-41758",
+  url: "https://autumn-dew-41758.botics.co",
+  api: "https://autumn-dew-41758.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
